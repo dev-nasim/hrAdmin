a76728a14daee1a5f989cc2f81b0c744697bc802
Anika <anishakabir09@gmail.com>
2022-11-10 13:14:52 +0600

    workinbg

--- app/Http/Controllers/Award/AwardController.php
+++ app/Http/Controllers/Award/AwardController.php
@@ -10,11 +10,7 @@ use Session;
 
 class AwardController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function index()
     {
         $data['awards'] = Award::paginate(10);
