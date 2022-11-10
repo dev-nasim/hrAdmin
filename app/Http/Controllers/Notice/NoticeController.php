14de8f9f689e8292f4e22c1eb93a668724979a58
nfjisan <mdzisanahmed83@gmail.com>
2022-11-10 15:13:00 +0600

    I am working with notice board

--- app/Http/Controllers/Notice/NoticeController.php
+++ app/Http/Controllers/Notice/NoticeController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers\Notice;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\notic;
+use Session;
 class NoticeController extends Controller
 {
-    
+
     public function index()
     {
-        return view('pages.notice.view_notice');
+        $data['notics'] = notic::get();
+        return view('pages.notice.view_notice',$data);
     }
 
     public function create()
@@ -20,7 +22,19 @@ class NoticeController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $this->validate($request,[
+            'notice_type' => 'required',
+            'description' => 'required',
+            'notice_date' => 'required',
+            'notice_by' => 'required',
+        ]);
+
+
+        $model = new notic();
+        $model->fill($request->all())->save();
+
+        Session::flash('success', 'notice Inserted');
+        return redirect('notice');
     }
 
     public function show($id)
