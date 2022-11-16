34ba5f895d5935d11dbbadb83ba872b8dbe602dd
Anika Tahseen <anishakabir09@gmail.com>
2022-11-16 15:12:03 +0600

    change add.employee

--- resources/views/pages/employee/add_employee.blade.php
+++ resources/views/pages/employee/add_employee.blade.php
@@ -50,16 +50,6 @@
                 </div>
 
                 <div class="form-group">
-                    <label for="award">Award:</label>
-                    <select class="form-control" name="award_id">
-                        <option value="">Select Award</option>
-                        @foreach($awards as $award)
-                            <option value="{{$award->id}}">{{$award->awd_name}}</option>
-                        @endforeach
-                    </select>
-                    <span class="text-danger">{{$errors->has('award_id') ? $errors->first('award_id') : ''}}</span>
-
-                <div class="form-group">
                     <label for="designation">Designation:</label>
                     <input type="text" name="designation" value="{{Request::old('designation')}}" class="form-control" id="designation">
                     <span class="text-danger">{{$errors->has('designation') ? $errors->first('designation') : ''}}</span>
